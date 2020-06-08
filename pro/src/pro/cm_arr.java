5589ea08a55f571de571bb17cef6c53e45e02d45
dhu758 <dhu758@naver.com>
2020-06-08 13:38:56 +0900

    cm 단위 변환

---
+++ pro/src/pro/cm_arr.java
@@ -0,0 +1,21 @@
+package pro;
+
+public class cm_arr {
+	public static void main(String[] args) {
+		double[] arr= {1, 0.01, 0.3937007874015748, 0.032808398950, 0.010936132983, 0.000006213712};
+		double[] ans_arr= {0,0,0,0,0,0};
+		
+		for(int i=0; i<arr.length; i++) {
+			double num=1/arr[i];
+			ans_arr[i]=Math.round(num*100)/100.0;
+		}
+		
+		
+		
+		for(int i=0; i<ans_arr.length; i++) {
+			for(int j=0; j<arr.length; j++)
+			System.out.printf("%-20.12f",ans_arr[i]*arr[j]);
+		System.out.println();
+		}
+	}
+}
