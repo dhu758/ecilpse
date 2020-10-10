891d4b2d9f816ac8808d7d6987d0a3df5704cc14
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-11 00:11:40 +0900

    트리 부모 찾기 -dfs

--- Basekjoon/src/dfs/ps11725.java
+++ Basekjoon/src/dfs/ps11725.java
@@ -1,12 +1,37 @@
 package dfs;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ps11725 {
-	static int N;
+	static int[] parents;
+	static ArrayList<Integer>[] list;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		
+		int N = sc.nextInt();
+		list = new ArrayList[N+1];
+		parents = new int[N+1];
+		for(int i=0; i<=N; i++) {
+			list[i] = new ArrayList<Integer>();
+		}
+		int a, b;
+		for(int i=0; i<N-1; i++) {
+			a = sc.nextInt();
+			b = sc.nextInt();
+			list[a].add(b);
+			list[b].add(a);
+		}
+		dfs(0, 1);
+		for(int i=2; i<=N; i++) {
+			System.out.println(parents[i]);
+		}
+	}
+	private static void dfs(int parent, int start) {
+		parents[start] = parent;
+		for(int item : list[start]) {
+			if(item != parent) {
+				dfs(start, item);
+			}
+		}
 	}
 }
