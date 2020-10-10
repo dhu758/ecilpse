09bad51bc3e1b6b3d8553d6a78e5b568c537fd10
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-10 23:36:13 +0900

    트리부모찾기-dfs

---
+++ Basekjoon/src/dfs/ps11725.java
@@ -0,0 +1,12 @@
+package dfs;
+
+import java.util.Scanner;
+
+public class ps11725 {
+	static int N;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		
+	}
+}
