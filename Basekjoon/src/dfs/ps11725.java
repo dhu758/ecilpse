370c973241a86661538ef973baa15147d15e6571
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-12 23:34:40 +0900

    트리 부모 찾기 -dfs

--- Basekjoon/src/dfs/ps11725.java
+++ Basekjoon/src/dfs/ps11725.java
@@ -10,27 +10,27 @@ public class ps11725 {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		list = new ArrayList[N+1];
-		parents = new int[N+1];
-		for(int i=0; i<=N; i++) {
+		for(int i=1; i<=N; i++) {
 			list[i] = new ArrayList<Integer>();
 		}
-		int a, b;
+		parents = new int[N+1];
+		int a,b;
 		for(int i=0; i<N-1; i++) {
-			a = sc.nextInt();
-			b = sc.nextInt();
+			a=sc.nextInt();
+			b=sc.nextInt();
 			list[a].add(b);
 			list[b].add(a);
 		}
-		dfs(0, 1);
+		dfs(1);
 		for(int i=2; i<=N; i++) {
 			System.out.println(parents[i]);
 		}
 	}
-	private static void dfs(int parent, int start) {
-		parents[start] = parent;
-		for(int item : list[start]) {
-			if(item != parent) {
-				dfs(start, item);
+	private static void dfs(int parent) {
+		for(int child:list[parent]) {
+			if(child!=parents[parent]&&parents[child]==0) {
+				parents[child]=parent;
+				dfs(child);
 			}
 		}
 	}
