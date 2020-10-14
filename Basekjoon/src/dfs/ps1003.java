588012f50d29cf9776f0ab242e491173ef5e39c0
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-15 00:21:58 +0900

    피보나치 함수-dp

--- Basekjoon/src/dfs/ps1003.java
+++ Basekjoon/src/dfs/ps1003.java
@@ -1,32 +1,34 @@
 package dfs;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ps1003 {
 	static int num0, num1;
 	static Integer[][] dp = new Integer[41][2];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		int number;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
 		dp[0][0]=1;
 		dp[0][1]=0;
 		dp[1][0]=0;
 		dp[1][1]=1;
-		Integer[][] answers = new Integer[T][2];
+		int number;
 		for(int i=0; i<T; i++) {
-			number = sc.nextInt();
-			answers[i] = fibonacciNum(number);
+			number = Integer.parseInt(br.readLine());
+			fibonacci(number);
+			sb.append(dp[number][0]+" "+dp[number][1]).append('\n');
 		}
-		for(int i=0; i<T; i++) {
-			System.out.println(answers[i][0]+" "+answers[i][1]);
-		}
+		System.out.println(sb);
 	}
-	private static Integer[] fibonacciNum(int n) {
-		if(dp[n][0] == null || dp[n][1] == null) {
-			dp[n][0] = fibonacciNum(n-1)[0] + fibonacciNum(n-2)[0];
-			dp[n][1] = fibonacciNum(n-1)[1] + fibonacciNum(n-2)[1];
+	private static Integer[] fibonacci(int number) {
+		if(dp[number][0] == null || dp[number][1] == null) {
+			dp[number][0] = fibonacci(number-1)[0] + fibonacci(number-2)[0];
+			dp[number][1] = fibonacci(number-1)[1] + fibonacci(number-2)[1];
 		}
-		return dp[n]; 
+		return dp[number];
 	}
+	
 }
