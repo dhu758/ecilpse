2303593b34393744b00a8925d0f5032ade2de9d2
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-22 00:08:55 +0900

    숨바꼭질 -dfs

--- Basekjoon/src/class3/ps1697.java
+++ Basekjoon/src/class3/ps1697.java
@@ -3,62 +3,95 @@ package class3;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
 public class ps1697 {
-	static int N;
-	static int K;
-	static int[] check = new int [100001];
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		K = sc.nextInt();
-		int answer;
-		if(N==K) {
-			answer=0;
-		}
-		else{
-			answer = bfs(N);
-		}
-		System.out.println(answer);
-	}
-<<<<<<< HEAD
-	private static int bfs(int N) {
-		Queue<Integer> queue = new LinkedList<>();
-		queue.add(N);
-		check[N] = 0;
-		
-		while(!queue.isEmpty()) {
-			int location = queue.poll();
-			int nextLocation;
-=======
-	private static void bfs(int num) {
-		Queue<Integer> q = new LinkedList<>();
-		q.add(num);
-		check[num]=1;
-		 
-		while(!q.isEmpty()) {
-			int temp = q.poll();
->>>>>>> refs/remotes/origin/master
-			for(int i=0; i<3; i++) {
-				if(i==0) {
-					nextLocation = location-1;
-				}
-				else if(i==1) {
-					nextLocation = location+1;
-				}
-				else {
-					nextLocation = 2*location;
-				}
-				if(nextLocation>=0&&nextLocation<=100000&&check[nextLocation]==0) {
-					check[nextLocation] = check[location]+1;
-					queue.add(nextLocation);
-					if(nextLocation==K) {
-						return check[nextLocation];
-					}
-				}
-			}
-		}
-		
-		return N;
-	}
+ static int N;
+ static int K;
+ static int[] check = new int [100001];
+ public static void main(String[] args){
+  Scanner sc = new Scanner(System.in);
+
+  N = sc.nextInt();
+
+  K = sc.nextInt();
+
+  int answer;
+
+  if(N==K) {
+
+   answer=0;
+
+  }
+
+  else{
+
+   answer = bfs(N);
+
+  }
+
+  System.out.println(answer);
+
+ }
+
+ private static int bfs(int N) {
+
+  Queue<Integer> queue = new LinkedList<>();
+
+  queue.add(N);
+
+  check[N] = 0;
+
+  
+
+  while(!queue.isEmpty()) {
+
+   int location = queue.poll();
+
+   int nextLocation;
+
+   for(int i=0; i<3; i++) {
+
+    if(i==0) {
+
+     nextLocation = location-1;
+
+    }
+
+    else if(i==1) {
+
+     nextLocation = location+1;
+
+    }
+
+    else {
+
+     nextLocation = 2*location;
+
+    }
+
+    if(nextLocation>=0&&nextLocation<=100000&&check[nextLocation]==0) {
+
+     check[nextLocation] = check[location]+1;
+
+     queue.add(nextLocation);
+
+     if(nextLocation==K) {
+
+      return check[nextLocation];
+
+     }
+
+    }
+
+   }
+
+  }
+
+  
+
+  return N;
+
+ }
+
 }
+
+
