b75184a281d1681f2f47cc02ea800545e89ee1ee
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-22 21:22:27 +0900

    듣보잡 - HashSet

--- Basekjoon/src/class3/ps1764.java
+++ Basekjoon/src/class3/ps1764.java
@@ -1,7 +1,35 @@
 package class3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class ps1764 {
 	public static void main(String[] args){
-		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		HashSet<String> hs = new HashSet<String>();
+		ArrayList<String> answer = new ArrayList<>();
+		sc.nextLine();
+		int num=0;
+		String st="";
+		for(int i=0; i<N; i++) {
+			st = sc.nextLine();
+			hs.add(st);
+		}
+		for(int i=0; i<M; i++) {
+			st = sc.nextLine();
+			if(hs.contains(st)) {
+				answer.add(st);
+				num++;
+			}
+		}
+		System.out.println(num);
+		Collections.sort(answer);
+		for(int i=0; i<answer.size(); i++) {
+			System.out.println(answer.get(i));
+		}
 	}
 }
