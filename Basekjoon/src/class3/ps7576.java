f67695c31ba61ab971ca78564e3dc7b3cecefe5d
DESKTOP-BMJ4529\heewon <dhu758@naver.com>
2020-10-27 00:56:13 +0900

    토마토 - bfs, new class

--- Basekjoon/src/class3/ps7576.java
+++ Basekjoon/src/class3/ps7576.java
@@ -19,42 +19,44 @@ public class ps7576 {
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
 				tomatoes[i][j] = sc.nextInt();
-				if(tomatoes[i][j]==1) {
+				if(tomatoes[i][j]!=0) {
 					num++;
-					q.add(new DOT(i,j));
 				}
-				if(tomatoes[i][j]==-1) {
-					num++;
+				if(tomatoes[i][j]==1) {
+					q.offer(new DOT(i,j));
 				}
 			}
 		}
 		if(num==N*M) {
-			answer=0;
+			answer = 0;
 		}
 		else {
 			answer=0;
 			bfs(tomatoes);
-			if(num!=N*M) {
-				answer = -1;
-			}
-			else if(num==N*M){
+			if(num==N*M) {
 				answer--;
+			}
+			else {
+				answer = -1;
 			}
 		}
 		System.out.println(answer);
+		
 	}
 	private static void bfs(int[][] tomatoes) {
-		int dx[] = {1,-1,0,0};
-		int dy[] = {0,0,1,-1};
+		int[] dx = {-1,1,0,0};
+		int[] dy = {0,0,-1,1};
 		while(!q.isEmpty()) {
 			DOT dot = q.poll();
+			int x = dot.x;
+			int y = dot.y;
 			for(int i=0; i<dx.length; i++) {
-				int nextX = dot.x+dx[i];
-				int nextY = dot.y+dy[i];
-				if(loc(nextX, nextY)&&tomatoes[nextX][nextY]==0) {
-					tomatoes[nextX][nextY] = tomatoes[dot.x][dot.y]+1;
+				int nextX = x+dx[i];
+				int nextY = y+dy[i];
+				if(loc(nextX,nextY)&&tomatoes[nextX][nextY]==0) {
+					tomatoes[nextX][nextY] = tomatoes[x][y]+1;
 					answer = tomatoes[nextX][nextY];
-					q.add(new DOT(nextX, nextY));
+					q.offer(new DOT(nextX, nextY));
 					num++;
 				}
 			}
@@ -71,7 +73,7 @@ class DOT{
 	int x;
 	int y;
 	
-	DOT(int x, int y){
+	DOT(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
